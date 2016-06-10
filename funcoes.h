2c7aeca12180d51b632a6df7d3010ed3a89717d3
Rafael Goto <rafael.fgoto@outlook.com>
2016-06-09 21:45:46 -0300

    Arquivo Header para facilitação do main.c

---
+++ funcoes.h
@@ -0,0 +1,13 @@
+#ifndef FUNCOES_H 
+#define FUNCOES_H
+
+double CF (double CF);
+double DBO (double DBO);
+double FT (double FT);
+double OD(double OD);
+double PH (double PH);
+double ST(double ST);
+double TU(double TU);
+double VT(double VT);
+
+#endif
