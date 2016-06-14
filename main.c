064197bb2f7eabb34f9a17d930f359d5223cac9d
Diego3010 <diego.moraes_sp@hotmail.com>
2016-06-14 16:50:41 -0300

    Adicionando a classificação do IQA

--- main.c
+++ main.c
@@ -42,5 +42,23 @@ IQA = pow(results[0], 0.15) * pow(results[1], 0.10) * pow(results[2], 0.10) * po
  * pow(results[5], 0.12) * pow(results[6], 0.08) * pow(results[7], 0.08) * pow(results[8], 0.10);
 
 printf("Valor de IQA = %f\n", IQA);
+printf("Classificação: ");
+
+if(IQA >= 80 && IQA <= 100){
+	printf("Ótima\n");
+
+} else if (IQA >= 51 && IQA < 80){
+	printf("Boa\n");
+
+} else if (IQA >= 37 && IQA < 51){
+	printf("Aceitável\n");
+
+} else if (IQA >= 20 && IQA < 37){
+	printf("Ruim\n");
+
+} else if (IQA >= 0 && IQA < 20){
+	printf("Péssima\n");
+} 
+
 
 }
